fce09705a137e5a80a00e5c0d7d4de96be5f92f5
vaibhav kamble <vaibhavkamble023@gmail.com>
2025-01-10 02:38:53 +0530

    Sync the database

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-const express = require('express');
-const cors = require('cors')
-const appRoute = require("./routes/index")
+const express = require("express");
+const cors = require("cors");
+const appRoute = require("./routes/index");
 
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
+const sequelize = require("./config/config");
 dotenv.config();
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use("/api",appRoute)
+app.use(express.json());
+app.use(cors());
+app.use("/api", appRoute);
 
-app.listen(process.env.PORT,()=>{
-    console.log("Server started on port 300");
-})
+
+(async () => {
+    try {
+      await sequelize.sync({ force: true }); // Set `alter: true` for production
+      console.log('Database synced successfully');
+    } catch (error) {
+      console.error('Error syncing database:', error);
+    }
+  })();
+
+  app.listen(3000,()=>{
+    console.log("server started");
+  })
